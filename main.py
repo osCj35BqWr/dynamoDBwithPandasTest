b473e3927ccbed483a70494f2fb33473482a7f96
osCj35BqWr <35219434+osCj35BqWr@users.noreply.github.com>
2020-11-22 21:35:11 +0900

    x軸に関して修正完了

--- main.py
+++ main.py
@@ -21,8 +21,9 @@ if __name__ == '__main__':
 
     df = pd.json_normalize(response["Items"])
 
-
+    #YYYYMMDDHHMMSS形式を日付として認識させる
     df.MeasureDateTime = pd.to_datetime(df.MeasureDateTime)
+
     # data["value"]はDecimalで入っているが。
     # Decimalは直接表示できないので、floatに変換
     df = df.astype({"value": float})
@@ -32,10 +33,19 @@ if __name__ == '__main__':
 
     fig, ax = plt.subplots()
 
-    # x軸の目盛りは1時間ごとにする
+    # x軸の目盛りは1時間ごとにする（set_major_locatorで目盛りを打つ場所を決める）
     ax.xaxis.set_major_locator(mdates.HourLocator())
-    # x軸の目盛りの表示形式を設定する
+    # x軸の目盛りの表示形式を設定する（set_major_formatterで目盛りに書く内容を決める）
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d\n%H:%M'))
 
     plt.plot(df.MeasureDateTime, df.value)
+    # ax.plot(df.MeasureDateTime, df.value)というのもある
+    plt.title('時間と人数の推移')
+    #plt.xlabel('X軸ラベル')
+    # plt.ylabel('人数')
+    plt.ylabel("人数", rotation=0)
+    # 縦書きにする場合
+    #ax.set_ylabel("人\n数", rotation=0, va='center')
+
+    plt.grid()
     plt.show()
